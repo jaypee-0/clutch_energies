cce7019616dba8a509708154d65aafba615c6d38
JAYPEE <eyibrajohnpaul@gmail.com>
2025-08-09 11:11:49 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0b0d0f] text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} text-white bg-black`}
       >
         {children}
       </body>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      
+      extend: {
+        colors: {
+          'primary': '#3DA451',
+          'secondary': '#FFFFFF',
+        },
+        backgroundColor: {
+          'primary': '#3DA451',
+          'secondary': '#FFFFFF',
+        },
+        backgroundImage: {
+          'hero-bg': "url('/assets/bg.png')",
+        }
+      },
+    },
+    plugins: [],
+  }
